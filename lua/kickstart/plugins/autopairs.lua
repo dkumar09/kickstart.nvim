fc0e079a4bd57774b099e00b71935f94c27fcc11
psyphi <psyphi@psyphi.psyphi>
2025-06-21 01:32:28 +0530

    clean autopairs

--- lua/kickstart/plugins/autopairs.lua
+++ lua/kickstart/plugins/autopairs.lua
@@ -5,17 +5,4 @@ return {
   'windwp/nvim-autopairs',
   event = 'InsertEnter',
   opts = {},
-  config = function()
-    require('nvim-autopairs').setup {
-      check_ts = true,
-    }
-    local npairs = require 'nvim-autopairs'
-    -- If you want to automatically add `(` after selecting a function or method
-    local cmp_autopairs = require 'nvim-autopairs.completion.cmp'
-    local cmp = require 'cmp'
-    cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
-
-    local Rule = require 'nvim-autopairs.rule'
-    npairs.add_rule(Rule('/*', '*/', { 'javascript', 'typescript', 'typescriptreact', 'javascriptreact' }))
-  end,
 }
